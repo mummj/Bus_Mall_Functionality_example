8e3d969b980220541b257a4c67893b4ae6fa9005
mummj <mummj@eou.edu>
2023-04-25 15:41:15 -0700

    added random image fuction to show 3 random images on refresh

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ const imgTitle1 = document.getElementById('img1title');
 const imgTitle2 = document.getElementById('img2title');
 const imgTitle3 = document.getElementById('img3title');
 
+let img1 = null;
+let img2 = null;
+let img3 = null;
 
 //Constructor function
-function Images(img, imgPath){
-    this.img = img;
+function Images(name, imgPath){
+    this.name = name;
     this.imgPath = imgPath;
     this.clicks = 0;
     this.votes = 0;
@@ -31,3 +34,33 @@ Images.allImages.push(new Images('Gym Pic', './img/Gym Picture.jpg'));
 Images.allImages.push(new Images('VBC Logo', './img/vbc logo.png'));
 Images.allImages.push(new Images('Vision Logo', './img/vision logo.jpg'));
 
+
+
+//Image Prototype that takes in the imgage and the name. 
+Images.prototype.renderImages = function(image, title){
+    image.src = this.imgPath;
+    title.textContent = this.img;
+    this.views ++;
+}
+
+//Randomizes all of the items in the array using the .sort function.
+function randomizeArray(){
+let myArray = Images.allImages;
+let randomArray = myArray.sort(function(){return (Math.random()-0.5)});
+console.log(randomArray)
+}
+
+//Get 3 images on reset
+function getThreeImages(){
+    randomizeArray();
+  let img1 = Images.allImages[0];
+  let img2 = Images.allImages[1];
+  let img3 = Images.allImages[2];
+    img1.renderImages(imgContainer1, imgTitle1);
+    img2.renderImages(imgContainer2, imgTitle2);
+    img3.renderImages(imgContainer3, imgTitle3);
+};
+
+
+
+getThreeImages();
